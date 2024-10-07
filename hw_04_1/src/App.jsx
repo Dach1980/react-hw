2380a4e8b8e3b61300b9a927a9ff4de8ff79cc9a
Dmitry <145884906+Dach1980@users.noreply.github.com>
2024-10-07 10:20:41 +1000

    Выполняет домашнее задание "Конвертер цветов"

---
+++ hw_04_1/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Hex2rgb from "./сomponents/Hex2rgb/Hex2rgb";
+import './App.css'
+function App() {
+    return (
+        <Hex2rgb></Hex2rgb>
+    );
+}
+export default App
