a0f24d3f4697b33d8c0337b1ab8723db8330ce50
Dmitry <145884906+Dach1980@users.noreply.github.com>
2024-11-04 20:52:24 +1000

    поправляет первую задачу

---
+++ hw_10_1_corrected/src/App.jsx
@@ -0,0 +1,13 @@
+import Form from './features/Form/Form';
+import Table from './features/Table/Table';
+
+function App() {
+  return (
+    <>
+      <Form />
+      <Table />
+    </>
+  );
+}
+
+export default App;
